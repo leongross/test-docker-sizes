c7c9831fd39060806bc2b4302e26e950b536247d
leongross <leon.gross@rub.de>
2023-01-24 18:19:01 +0100

    update script, add readme

--- build_docker_files.sh
+++ build_docker_files.sh
@@ -12,15 +12,23 @@ file_sizes=(10000 10000000 100000000)
 echo "Generating payloads"
 for i in "${file_sizes[@]}";do
     echo "[*] Payload size = $i"
-    dd if=/dev/zero of=file_"$i".bin bs=1 count=$i
+    file=file_"$i".bin
+    dd if=/dev/zero of="$file" bs=1 count="$i"
 
     DOCKER_BUILDKIT=1 \
     docker build \
-        --build-arg TAG=file_"$i" \
+        --build-arg TAG=file_"$file" \
         -t "$upstream":"$i" \
         .
     
+    echo "[*] Pushing to $upstream:$i"
     docker push "$upstream:$i"
+
+    echo "[*] Deleting local image $upstream:$i"
+    docker image rm "$upstream:$i"
+    
+    echo "[*] Deleting file file_$i"
+    rm "$file"
 done
 
 
